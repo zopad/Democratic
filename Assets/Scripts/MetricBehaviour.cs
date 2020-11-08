a87b8e0921a03da795b0fbfc76a8358abbc802f2
Zoboki Árpád <awd000@gmail.com>
2020-11-08 19:02:22 +0100

    Init

---
+++ Assets/Scripts/MetricBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MetricBehaviour : MonoBehaviour
+{
+
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut() {
+        for (float i = 1; i >=0; i -=Time.deltaTime) {
+            _spriteRenderer.material.color = new Color(1,1,1,i);
+            yield return null;
+        }
+    }
+}
